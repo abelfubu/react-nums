a4db8940751fe54596cbd8a096b6943d5eb335b3
abelfubu <abelfubu@gmail.com>
2020-06-17 10:27:27 +0200

    Updates

---
+++ precache-manifest.566e111d2109c4db1b9e0f90c8330a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "784d897408a51fff6262f3d4a2d6050c",
+    "url": "/react-nums/index.html"
+  },
+  {
+    "revision": "e47d1d60075485e9e98c",
+    "url": "/react-nums/static/css/main.ca2b6aca.chunk.css"
+  },
+  {
+    "revision": "6af300b9442c43c90c6a",
+    "url": "/react-nums/static/js/2.ab7a6dc0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-nums/static/js/2.ab7a6dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47d1d60075485e9e98c",
+    "url": "/react-nums/static/js/main.560dfd28.chunk.js"
+  },
+  {
+    "revision": "31db9141ffd1d73ebb65",
+    "url": "/react-nums/static/js/runtime-main.02648d9a.js"
+  }
+]);
